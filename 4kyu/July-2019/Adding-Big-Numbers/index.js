4f01b5fbd9027f94da6961cf2382012632ffeeed
Manuel Garaboa <mannygaraboa@gmail.com>
2019-07-11 12:16:55 -0400

    Adding Big Numbers Kata Start

---
+++ 4kyu/July-2019/Adding-Big-Numbers/index.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  let sum = 0;
+  let numA = parseInt(a, 10);
+  let numB = parseInt(b, 10);
+  sum = numA + numB;
+  return sum.toString();
+}
+add("112345678901234567890", "21234567890123456789098765432345")
