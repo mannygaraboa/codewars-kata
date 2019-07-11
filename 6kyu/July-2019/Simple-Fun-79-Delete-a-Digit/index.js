1c44f52dfe6c6a3c6e331a0f74915ee2252cefe0
Manuel Garaboa <mannygaraboa@gmail.com>
2019-07-11 12:00:54 -0400

    Delete a Digit Kata Solution

--- 6kyu/July-2019/Simple-Fun-79-Delete-a-Digit/index.js
+++ 6kyu/July-2019/Simple-Fun-79-Delete-a-Digit/index.js
@@ -1,3 +1,36 @@
 function deleteDigit(n) {
+  // Split all digits in n into an Array
+  let arr = [];
+  let numbersArr = [];
+  let nString = n.toString();
+  for(let i = 0; i < nString.length; i++)
+  {
+    let currentString = nString[i];
+    let currentNum = parseInt(currentString, 10);
+    arr.push(currentNum)
+  }
+  console.log(arr)
   
+  // Makes array of all posibilites after removing one number at a time
+  for(let i = 0; i < arr.length; i++)
+  {
+    let arrCopy = arr.slice(0);
+    let removed = arrCopy.splice(i,1);
+    let newNumString = arrCopy.join("");
+    let newNum = parseInt(newNumString, 10);
+    numbersArr.push(newNum);
+  }
+  console.log(numbersArr);
+
+  // Sort in Descending Order:
+  numbersArr.sort(function(a,b) {return b-a});
+  console.log("Sorted: " + numbersArr);
+  return numbersArr[0];
 }
+deleteDigit(447258)
+
+// Other Solution:
+  // function deleteDigit(n) {
+  //   const s = String(n)
+  //   return Math.max(...Array.from(s, (_, i) => s.slice(0, i) + s.slice(i + 1)))
+  // }
