003a949d50ca15880b33b66a9ad97e55b1258fdd
Manuel Garaboa <mannygaraboa@gmail.com>
2019-07-15 17:10:44 -0400

    Testing in kata

--- 6kyu/July-2019/Decoded-String-by-Numbers/index.js
+++ 6kyu/July-2019/Decoded-String-by-Numbers/index.js
@@ -1,6 +1,22 @@
 function decode(str) {
   var result = [];
-  
-  
+
+  for(let i = 0; i < str.length; i++)
+  {
+    result.push(str.charAt(i));
+  }
+
   return result;
 }
+decode("abc\5defghi\2jkl")
+
+// if(currentChar.match(/[a-z]/i))
+// {
+//   result.push(currentChar);
+// }
+
+// if (isNaN(parseInt(currentChar, 10))) 
+// {
+//   console.log(currentChar);
+//   result.push(currentChar);
+// }
