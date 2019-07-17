d74fa4c4dd91288153153726e14d3f606b94bf66
Manuel Garaboa <mannygaraboa@gmail.com>
2019-07-17 17:21:12 -0400

    Decoded String by Numbers kata solution

--- 6kyu/July-2019/Decoded-String-by-Numbers/index.js
+++ 6kyu/July-2019/Decoded-String-by-Numbers/index.js
@@ -12,15 +12,24 @@ function decode(str) {
   // For numbers higher than double digits, pushed into second array
   for(let i = 0; i < firstArr.length; i++)
   {
-    if(firstArr[i] == "\\")
+    let newNumChar = firstArr[i+1];
+    let newNum = parseInt(newNumChar, 10);
+    if(firstArr[i] == "\\" && newNum >= 0)
     {
-      let newNum = firstArr[i+1];
+      secondArr.push(firstArr[i])
       while(firstArr[i+2] >= 0)
       {
-        newNum = newNum.concat(firstArr[i+2]);
+        newNumChar = newNumChar.concat(firstArr[i+2]);
         i++;
       }
-      secondArr.push(newNum);
+      secondArr.push(newNumChar);
+      let count = 0;
+      while(count < newNum)
+      {
+        i++;
+        secondArr.push(firstArr[i+1])
+        count++;
+      }
       i++;
     }
     else
@@ -33,29 +42,56 @@ function decode(str) {
   // Grouping together elements to form new characters based off numbers within array
   for(let i = 0; i < secondArr.length; i++)
   {
-    if(secondArr[i] > 0)
+    if(secondArr[i] == "\\" && secondArr[i+1] > 0)
     {
-      let num = parseInt(secondArr[i], 10)
+      let num = parseInt(secondArr[i+1], 10)
       let count = 0;
       let newChar = "";
       while(count < num)
       {
-        newChar = newChar.concat(secondArr[i+1]);
+        let currentChar = secondArr[i+2]
+        if(currentChar == undefined)
+        {
+          break;
+        }
+        if(currentChar != undefined)
+        {
+          newChar = newChar.concat(currentChar);
+        }
+        if(currentChar.length > 1)
+        {
+          // console.log(currentChar);
+          num = num - (currentChar.length - 1);
+        }
         i++;
         count++;
       }
-      //console.log(newChar);
       result.push(newChar);
+      i++;
     }
     else
     {
       result.push(secondArr[i])
     }
   }
-
   return result;
 }
-decode("abc\\10defghi\\2jklkdow\\4lerpfigf")
+decode("\\12cvalQDjZZGynP\\66X8T\\3nXDLujYgxlWopLSQgckQuohQASBnJLsb\\9QUxBPHSRe\\37as0ug4QjoN2ma3IcLTpNmLwrxFB\\65zzW5hPWqV")
+
+// Other Solution:
+  // function decode(str) {
+  //     for (var i = 0, res = []; i < str.length; i++)
+  //         if (str[i] === '\\') {
+  //             var start = ++i;
+  //             while (!isNaN(str[i])) i++;
+  //             var num = +str.slice(start, i);
+  //             res.push(str.slice(i, i + num));
+  //             i += --num;
+  //             continue;
+  //         }
+  //         else res.push(str[i]);
+  //     return res;
+  // }
 
 // Past Methods: 
   // if(currentChar.match(/[a-z]/i))
