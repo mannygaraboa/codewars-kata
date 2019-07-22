bff5d7ec3ece55cb99bf5b9e1322301393a31d79
Manuel Garaboa <mannygaraboa@gmail.com>
2019-07-22 11:09:32 -0400

    Sum of Pairs solution, still requires codewars optimization

--- 5kyu/July-2019/Sum-of-Pairs/index.js
+++ 5kyu/July-2019/Sum-of-Pairs/index.js
@@ -1,26 +1,40 @@
 var sum_pairs=function(ints, s){
   console.log("All integers: " + ints);
   console.log("Sum: " + s);
-  let pair = [];
+  let count = 1;
   for(let i = 0; i < ints.length; i++)
   {
-    let currentNum = ints[i];
-    for(let j = i+1; j < ints.length; j++)
+    if((i + count) == (ints.length))
     {
-      let currentNum2 = ints[j];
-      if(currentNum + currentNum2 == s)
-      {
-        pair.push(currentNum);
-        pair.push(currentNum2);
-        // console.log(pair);
-        return pair;
-      }
+      i = 0;
+      count++;
+      console.log("count is now: " + count);
+    }
+    let firstNum = ints[i];
+    let secondNum = ints[i+count];
+    console.log(firstNum + " + " + secondNum);
+    if(firstNum + secondNum == s)
+    {
+      return [firstNum, secondNum];
+    }
+    
+    if(count == ints.length)
+    {
+      console.log("No pair of numbers can be added to sum " + s);
+      return undefined
     }
   }
-  if(pair.length == 0)
-  {
-    console.log("No pair of numbers can be added to sum " + s);
-    return undefined
-  }
 }
-sum_pairs([10, 5, 2, 3, 7, 5], 10)
+sum_pairs([1, 4, 8, 8, 3, 15], 19)
+
+// for(let j = i+1; j < ints.length; j++)
+// {
+//   let currentNum2 = ints[j];
+//   if(currentNum + currentNum2 == s)
+//   {
+//     pair.push(currentNum);
+//     pair.push(currentNum2);
+//     // console.log(pair);
+//     return pair;
+//   }
+// }
