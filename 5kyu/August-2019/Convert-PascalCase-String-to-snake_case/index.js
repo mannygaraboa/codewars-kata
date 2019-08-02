5c9cdb7ce49b74ceac9ac9eec6720b0ad8e8b35f
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-02 16:58:03 -0400

    Convert PascalCase String to snake_case kata solution

--- 5kyu/August-2019/Convert-PascalCase-String-to-snake_case/index.js
+++ 5kyu/August-2019/Convert-PascalCase-String-to-snake_case/index.js
@@ -1,3 +1,35 @@
 function toUnderscore(string) {
-  // TODO: complete 
+  let stringArray = [];
+  if(string >= 0)
+  {
+    string = string.toString();
+  }
+  for(i = 0; i < string.length; i++)
+  {
+    let currentChar = string.charAt(i);
+    if(currentChar.match(/[a-z]/i))
+    {
+      if(currentChar === currentChar.toUpperCase() && i == 0)
+      {
+        currentChar = currentChar.toLowerCase();
+      }
+      else if(currentChar === currentChar.toUpperCase() && i > 0)
+      {
+        stringArray.push("_");
+        currentChar = currentChar.toLowerCase();
+      }
+    }
+    stringArray.push(currentChar);
+  }
+  console.log(stringArray);
+  let snake_string = stringArray.join("");
+  return snake_string;
 }
+toUnderscore(1); // ==> test_controller
+
+// Other Solution:
+  // var toUnderscore;
+
+  //   toUnderscore = function(string) {
+  //     return string.toString().split(/(?=[A-Z])/).join('_').toLowerCase();
+  //   };
