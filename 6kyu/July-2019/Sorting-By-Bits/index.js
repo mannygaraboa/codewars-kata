54f3f4e502381adf6f9bec051fcb52f6ff5bf2d4
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-02 16:11:03 -0400

    Sorting by Bits kata solution & Convert PascalCase String to snake_case kata start

--- 6kyu/July-2019/Sorting-By-Bits/index.js
+++ 6kyu/July-2019/Sorting-By-Bits/index.js
@@ -17,33 +17,43 @@ function sortByBit(arr) {
       }
     }
     
+    // Pushing pair into bitArray
     let pair = [num, bitCount];
     bitArray.push(pair);
   }
-  // Clear original arr
-  arr.length = 0;
 
   // Sorting bitArray by number of bitCount
   bitArray.sort( function(a, b)
   {
     if ( a[0] == b[0] ) return 0;
-    return a[0] < b[0] ? 1 : -1;
+    return a[0] < b[0] ? -1 : 1;
   });
+  // console.log(bitArray);
   bitArray.sort( function(a, b)
   {
-    if ( a[0] < b[0] && a[1] == b[1] ) return 0;
+    if ( a[0] < b[0] && a[1] == b[1] ) return -1;
     return a[1] < b[1] ? -1 : 1;
   });
   // console.log(bitArray);
 
-  // Pushing first index of each array within bitArray into the original arr
+  // Pushing first index of each array within bitArray into arrCopy
+  let arrCopy = [];
   for(let i = 0; i < bitArray.length; i++)
   {
-    arr.push((bitArray[i])[0]);
+    arrCopy.push((bitArray[i])[0]);
   }
+  // console.log(arrCopy);
+
+  // Sort original arr into the same way as arrCopy
+  arr.sort(function(a, b){  
+    return arrCopy.indexOf(a) - arrCopy.indexOf(b);
+  });
   return arr;
 }
-sortByBit([9,4,5,3,5,7,2,56,8,2,6,8,0])
+sortByBit([9,4,5,3,5,7,2,56,16,8,2,6,8,0])
 
 // Bitwise Operator:
   // console.log(5 & 13 & 3); ==> returns 1
+
+// Other Solution:
+  // const sortByBit = arr => arr.sort((a, b) => a.toString(2).replace(/0/g, '') - b.toString(2).replace(/0/g, '') || a - b)
