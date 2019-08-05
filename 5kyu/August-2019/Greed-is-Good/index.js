0cffc4daadcc1675022de32893213f07aa9cc825
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-05 18:18:41 -0400

    Greed is Good kata optimized solution

--- 5kyu/August-2019/Greed-is-Good/index.js
+++ 5kyu/August-2019/Greed-is-Good/index.js
@@ -1,102 +1,71 @@
 function score( dice ) {
   let score = 0;
-  let arr1 = [];
-  let arr2 = [];
-  let arr3 = [];
-  let arr4 = [];
-  let arr5 = [];
-  let arr6 = [];
+  let pairs = [];
 
   for(let i = 0; i < dice.length; i++)
   {
-    if(dice[i] == 1)
+    let num = dice[i];
+    let numCount = 1;
+    for(let j = i+1; j < dice.length; j++)
     {
-      arr1.push(dice[i]);
+      if(num == dice[j])
+      {
+        numCount++;
+        dice.splice(j, 1);
+        j--;
+      }
     }
-    else if(dice[i] == 2)
-    {
-      arr2.push(dice[i]);
-    }
-    else if(dice[i] == 3)
-    {
-      arr3.push(dice[i]);
-    }
-    else if(dice[i] == 4)
-    {
-      arr4.push(dice[i]);
-    }
-    else if(dice[i] == 5)
-    {
-      arr5.push(dice[i]);
-    }
-    else if(dice[i] == 6)
-    {
-      arr6.push(dice[i]);
-    }
+    pairs.push([num, numCount])
   }
+  console.log(pairs);
 
-  console.log(arr1);
-  console.log(arr2);
-  console.log(arr3);
-  console.log(arr4);
-  console.log(arr5);
-  console.log(arr6);
-
-  while(arr1.length > 0)
+  for(let i = 0; i < pairs.length; i++)
   {
-    if(arr1.length >= 3)
+    if(pairs[i][0] == 1)
     {
-      score += 1000;
-      arr1.splice(0, 3);
+      if(pairs[i][1] >= 3)
+      {
+        score += 1000;
+        score += ((pairs[i][1] - 3) * 100);
+      }
+      else
+      {
+        score += ((pairs[i][1]) * 100)
+      }
     }
-    else
+    else if(pairs[i][0] == 2 && pairs[i][1] >= 3)
     {
-      score += 100;
-      arr1.splice(0, 1);
+      score += 200;
     }
-  }
-
-  while(arr2.length >= 3)
-  {
-    score += 200;
-    arr2.splice(0, 3);
-  }
-
-  while(arr3.length >= 3)
-  {
-    score += 300;
-    arr3.splice(0, 3);
-  }
-
-  while(arr4.length >= 3)
-  {
-    score += 400;
-    arr4.splice(0, 3);
-  }
-
-  while(arr5.length > 0)
-  {
-    if(arr5.length >= 3)
+    else if(pairs[i][0] == 3 && pairs[i][1] >= 3)
     {
-      score += 500;
-      arr2.splice(0, 3);
+      score += 300;
     }
-    else
+    else if(pairs[i][0] == 4 && pairs[i][1] >= 3)
     {
-      score += 50;
-      arr5.splice(0, 1);
+      score += 400;
     }
-  }
-
-  while(arr6.length >= 3)
-  {
-    score += 600;
-    arr4.splice(0, 3);
+    else if(pairs[i][0] == 5)
+    {
+      if(pairs[i][1] >= 3)
+      {
+        score += 500;
+        score += ((pairs[i][1] - 3) * 50);
+      }
+      else
+      {
+        score += ((pairs[i][1]) * 50)
+      }
+    }
+    else if(pairs[i][0] == 6 && pairs[i][1] >= 3)
+    {
+      score += 600;
+    }
   }
 
   return score;
 }
-score( [1, 1, 1, 3, 1] );
+score( [4, 1, 4, 4, 5] );
 
 // Point Values:
   // Three 1's => 1000 points
@@ -107,3 +76,14 @@ score( [1, 1, 1, 3, 1] );
   // Three 2's =>  200 points
   // One   1   =>  100 points
   // One   5   =>   50 point
+
+// Other Solution:
+  // function score( dice ) {
+  //   var dc = [0,0,0,0,0,0];
+  //   var tdr = [1000,200,300,400,500,600];
+  //   var sdr = [100,0,0,0,50,0];
+  //   dice.forEach(function(x){ dc[x-1]++; });
+  //   return dc.reduce(function(s,x,i){ 
+  //     return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
+  //   },0);
+  // }
