c966f101fed3f896b94282cdc0b4b007d84b68c9
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-13 16:24:25 -0400

    InterlacedSpiralCipher.decode function solution (hard-coded)

--- 5kyu/August-2019/Interlaced-Spiral-Cipher/index.js
+++ 5kyu/August-2019/Interlaced-Spiral-Cipher/index.js
@@ -2,6 +2,10 @@ const InterlacedSpiralCipher = {};
 let phrase1 = `Romani ite domum`;
 let phrase2 = `Stsgiriuar i ninmd l otac`;
 
+/*--------------------------------------------------------------------------------------*/
+/*----------------------------------------ENCODE----------------------------------------*/
+/*--------------------------------------------------------------------------------------*/
+
 InterlacedSpiralCipher.encode = function(str){
   // Insert function to properly place characters
   Array.prototype.insert = function ( index, item ) {
@@ -100,9 +104,9 @@ InterlacedSpiralCipher.encode = function(str){
     
     // Combine the row arrays & then concat them together
     let concat = [].concat.apply([], square);
-    let join = concat.join("");
-    console.log(join);
-    return join;
+    let string = concat.join("");
+    console.log(string);
+    return string;
   }
 
   // For a 5 x 5 square
@@ -224,15 +228,110 @@ InterlacedSpiralCipher.encode = function(str){
 
     // Combine the row arrays & then concat them together
     let concat = [].concat.apply([], square);
-    let join = concat.join("");
-    console.log(join);
-    return join;
+    let string = concat.join("");
+    console.log(string);
+    return string;
   }
 };
 
+
+/*--------------------------------------------------------------------------------------*/
+/*----------------------------------------DECODE----------------------------------------*/
+/*--------------------------------------------------------------------------------------*/
+
+
 InterlacedSpiralCipher.decode = function(str){
-  	
+  // Insert function to properly place characters
+  Array.prototype.insert = function ( index, item ) {
+    this.splice( index, 0, item );
+  };
+  // Find out number of rows to make a perfect square
+	console.log("String length = " + str.length);
+  let square = [];
+  let sqrt = Math.sqrt(str.length);
+  let rows = Math.ceil(sqrt);
+  
+  for(let i = 0; i < rows; i++)
+  {
+    square.push([]);
+  }
+  console.log(square);
+
+  // Adding characters to rows for a 4 x 4 square
+  if(rows == 4)
+  {
+    for(let i = 0; i < 16; i++)
+    {
+      let currentChar = str.charAt(i);
+      if(square[0].length < 4)
+      {
+        square[0].push(currentChar);
+      }
+      else if(square[1].length < 4 && square[0].length == 4)
+      {
+        square[1].push(currentChar);
+      }
+      else if(square[2].length < 4 && square[1].length == 4)
+      {
+        square[2].push(currentChar);
+      }
+      else if(square[3].length < 4 && square[2].length == 4)
+      {
+        square[3].push(currentChar);
+      }
+    }
+    console.log(square);
+
+    let string = 
+    square[0][0] + square[0][3] + square[3][3] + square[3][0]
+    + square[0][1] + square[1][3] + square[3][2] + square[2][0]
+    + square[0][2] + square[2][3] + square[3][1] + square[1][0]
+    + square[1][1] + square[1][2] + square[2][2] + square[2][1]
+    console.log(string);
+    return string;
+  }
+
+  // Adding characters to rows for a 5 x 5 square
+  if(rows == 5)
+  {
+    for(let i = 0; i < 25; i++)
+    {
+      let currentChar = str.charAt(i);
+      if(square[0].length < 5)
+      {
+        square[0].push(currentChar);
+      }
+      else if(square[1].length < 5 && square[0].length == 5)
+      {
+        square[1].push(currentChar);
+      }
+      else if(square[2].length < 5 && square[1].length == 5)
+      {
+        square[2].push(currentChar);
+      }
+      else if(square[3].length < 5 && square[2].length == 5)
+      {
+        square[3].push(currentChar);
+      }
+      else if(square[4].length < 5 && square[3].length == 5)
+      {
+        square[4].push(currentChar);
+      }
+    }
+    console.log(square);
+
+    let string = 
+    square[0][0] + square[0][4] + square[4][4] + square[4][0]
+    + square[0][1] + square[1][4] + square[4][3] + square[3][0]
+    + square[0][2] + square[2][4] + square[4][2] + square[2][0]
+    + square[0][3] + square[3][4] + square[4][1] + square[1][0]
+    + square[1][1] + square[1][3] + square[3][3] + square[3][1]
+    + square[1][2] + square[2][3] + square[3][2] + square[2][1]
+    + square[2][2];
+    console.log(string);
+    return string;
+  }
 };
 
 InterlacedSpiralCipher.encode(phrase1); // `Rntodomiimuea  m`
-InterlacedSpiralCipher.decode(phrase2); // `Sic transit gloria mundi` 
+InterlacedSpiralCipher.decode(phrase2); // `Sic transit gloria mundi ` 
