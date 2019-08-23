2a1ffc2e4ef773677c67cb935bbe56b8f98ad29b
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-23 17:24:01 -0400

    Meeting kata start

---
+++ 6kyu/August-2019/Meeting/index.js
@@ -0,0 +1,15 @@
+function meeting(s) {
+  for(let i = 0; i < s.length; i++)
+  {
+    let firstName = "";
+    let lastName = "";
+    if(s.charAt(i) == ":")
+    {
+      console.log(s.charAt(i));
+    }
+
+    
+  }
+  console.log(s);
+}
+meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull");
