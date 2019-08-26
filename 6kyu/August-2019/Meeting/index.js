8e04e9413041b8e97acbfc186357b0930d689fcd
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-26 17:09:24 -0400

    Meeting kata progress

--- 6kyu/August-2019/Meeting/index.js
+++ 6kyu/August-2019/Meeting/index.js
@@ -1,15 +1,39 @@
 function meeting(s) {
+  s = s.toUpperCase();
+  let nameList = [];
+  let name = "";
+  let fullName = [];
   for(let i = 0; i < s.length; i++)
   {
-    let firstName = "";
-    let lastName = "";
-    if(s.charAt(i) == ":")
+    if(s.charAt(i) != ":" && s.charAt(i) != ";")
     {
-      console.log(s.charAt(i));
+      name = name.concat(s.charAt(i));
+      // Push LAST last name 
+      if(i == s.length - 1)
+      {
+        fullName.push(name);
+      }
+    }
+    else if(s.charAt(i) == ":")
+    {
+      fullName.push(name);
+      name = "";
+    }
+    else if(s.charAt(i) == ";")
+    {
+      fullName.push(name);
+      name = "";
     }
 
-    
+    if(fullName.length == 2)
+    {
+      nameList.push(fullName);
+      fullName = [];
+    }
   }
-  console.log(s);
+
+  nameList.sort()
+
+  console.log(nameList);
 }
 meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull");
