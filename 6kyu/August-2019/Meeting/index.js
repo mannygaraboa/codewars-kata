fb2ae755531e5e8e4840731a24b496f914540c13
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-27 13:12:28 -0400

    Meeting kata solution

--- 6kyu/August-2019/Meeting/index.js
+++ 6kyu/August-2019/Meeting/index.js
@@ -1,8 +1,10 @@
 function meeting(s) {
   s = s.toUpperCase();
+  console.log(s);
   let nameList = [];
   let name = "";
   let fullName = [];
+  let finalString = "";
   for(let i = 0; i < s.length; i++)
   {
     if(s.charAt(i) != ":" && s.charAt(i) != ";")
@@ -32,8 +34,33 @@ function meeting(s) {
     }
   }
 
-  nameList.sort()
+  nameList.sort(function(a, b) { 
+    if ( a[0] == b[0] ) return 0;
+    return a[0] < b[0] ? -1 : 1;
+  });
+  nameList.sort(function(a, b) { 
+    if ( a[0] < b[0] && a[1] == b[1] ) return -1;
+    return a[1] < b[1] ? -1 : 1;
+  });
+
+  for(let i = 0; i < nameList.length; i++)
+  {
+    finalString = finalString.concat("(" + nameList[i][1] + ", " + nameList[i][0] + ")");
+  }
 
   console.log(nameList);
+  return finalString;
 }
-meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull");
+meeting("Anna:Wahl;Grace:Gates;James:Russell;Elizabeth:Rudd;Victoria:STAN;Jacob:Wahl;Alex:Wahl;Antony:Gates;Alissa:Meta;Megan:Bell;Amandy:Stan;Anna:Steve");
+
+/*  
+  Other Solution:
+
+  function meeting(s) {
+    let string = s.toUpperCase().split(';')
+                  .map(x => x.split(':').reverse().join(', '))
+                  .sort()
+                  .join(')(')
+    return '(' + string + ')'
+  }
+*/
