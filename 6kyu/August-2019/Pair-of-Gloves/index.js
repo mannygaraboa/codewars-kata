2fb23e068e823a863fe029d018cc368a1dafe900
Manuel Garaboa <mannygaraboa@gmail.com>
2019-08-29 17:41:53 -0400

    Pair of Gloves kata solution

--- 6kyu/August-2019/Pair-of-Gloves/index.js
+++ 6kyu/August-2019/Pair-of-Gloves/index.js
@@ -1,19 +1,59 @@
 function numberOfPairs(gloves)
 {
+  console.log(gloves);
   let pairs = 0;
+  let colorSort = [];
   for(let i = 0; i < gloves.length; i++)
   {
-    for(let j = i+1; j < gloves.length; j++)
+    let currentColor = gloves[i];
+    let bool = true;
+    for(let j = 0; j < colorSort.length; j++)
     {
-      if(gloves[i] == gloves[j])
+      if(currentColor == colorSort[j][0])
       {
-        pairs++;
-        gloves.splice(j,1);
-        gloves.splice(i,1);
-        i--;
+        bool = false;
       }
     }
+    let colorCount = 1;
+    for(let j = i+1; j < gloves.length; j++)
+    {
+      if(currentColor == gloves[j])
+      {
+        colorCount++;
+      }
+    }
+    let pair = [currentColor, colorCount];
+    if(bool == true)
+    {
+      colorSort.push(pair);
+    }
+  }
+
+  console.log(colorSort);
+
+  for(let i = 0; i < colorSort.length; i++)
+  {
+    let pairedGloves = colorSort[i][1] / 2;
+    pairedGloves = Math.floor(pairedGloves);
+    pairs += pairedGloves;
   }
   return pairs;
 }
-numberOfPairs(['red','green','red','blue','blue']);
+numberOfPairs(['red','green','red','blue','blue', 'red', 'red']);
+
+/*
+  Other Solution:
+
+  function numberOfPairs(gloves)
+  {
+    gloves = gloves.sort();
+    let count = 0;
+    for (var i = 0; i < gloves.length; i++) {
+      if (gloves[i] === gloves[i + 1]) {
+        count++
+        i++
+      }
+    }
+    return count;
+  }
+*/
