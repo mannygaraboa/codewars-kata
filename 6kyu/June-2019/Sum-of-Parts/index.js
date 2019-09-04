8cdc5dbfd3397388ac81535a8240d5426ad95f57
Manuel Garaboa <mannygaraboa@gmail.com>
2019-09-03 23:17:39 -0400

    Sum of Parts kata new approach start

--- 6kyu/June-2019/Sum-of-Parts/index.js
+++ 6kyu/June-2019/Sum-of-Parts/index.js
@@ -17,11 +17,30 @@ function partsSums(ls) {
 }
 partsSums([1,3,5,6,2])
 
-// ls.shift();
+// Attempting new approach:
+function partsSums(ls)
+{
+  let sumArray = [];
+  for(let i = 0; i < ls.length; i++)
+  {
+    let sum = ls[i];
+    for(let j = i + 1; j < ls.length; j++)
+    {
+      sum += ls[j];
+    }
+    sumArray.push(sum);
+  }
+  sumArray.push(0);
+  return sumArray;
+}
+partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358])
 
-// while(ls.length > 0)
-// {
-//   console.log(ls);
-//   sumArray.push(ls);
-//   ls.shift();
-// }
+// Other Methods:
+  // ls.shift();
+
+  // while(ls.length > 0)
+  // {
+  //   console.log(ls);
+  //   sumArray.push(ls);
+  //   ls.shift();
+  // }
