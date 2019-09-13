c4b65aeaff54d51cfee492004187b9d21680344c
Manuel Garaboa <mannygaraboa@gmail.com>
2019-09-13 17:43:06 -0400

    Testing new method for kata solution

--- 6kyu/May-2019/is-a-prime-number?/index.js
+++ 6kyu/May-2019/is-a-prime-number?/index.js
@@ -16,5 +16,31 @@ function isPrime(num) {
     i++
   }
   return bool;
+
+  // New Method Attempt:
+  function isPrime(num) {
+    let bool = true;
+    if(num <= 1 || num == 4)
+    {
+      bool = false;
+      return bool;
+    }
+    
+    if(num == 2 || num == 3 || num == 5)
+    {
+      bool = true;
+    }
+    
+    if(num >= 6)
+    {
+      if(num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 11 == 0 || num % 13 == 0 || num % 17 == 0 || num % 19 == 0)
+      {
+        bool = false;
+      }
+    }
+    
+    return bool;
+  }
+  isPrime(7);
 }
 isPrime(25)
