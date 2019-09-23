ec96caafdb335218e2e94aa1522dda39ce1797ed
Manuel Garaboa <mannygaraboa@gmail.com>
2019-09-23 16:46:17 -0400

    is a prime number kata solution

--- 6kyu/May-2019/is-a-prime-number?/index.js
+++ 6kyu/May-2019/is-a-prime-number?/index.js
@@ -1,56 +1,32 @@
 function isPrime(num) {
   let bool = true;
-  if(num <= 1)
+  let sqrt = Math.sqrt(num);
+
+  if(num <= 1 || Number.isInteger(sqrt) == true)
   {
     bool = false;
     return bool;
   }
+
   let i = 2;
-  while(i < num)
+  while(i < sqrt)
   {
     if(num % i == 0)
     {
       bool = false;
-      break;
+      return bool;
     }
-    i++
+    i++;
   }
+  
   return bool;
 }
-  // New Method Attempt:
-function isPrime(num) {
-  let bool = true;
-  if(num <= 1)
-  {
-    bool = false;
-    return bool;
-  }
-  else if(num % 2 == 0 && num != 2)
-  {
-    bool = false;
-    return bool;
-  }
-  else if(num % 2 != 0)
-  {
-    if(num % 3 == 0 && num != 3 || num % 5 == 0 && num != 5 || num % 7 == 0 && num != 7)
-    {
-      bool = false;
-      return bool;
-    }
-    
-    let sqrt = Math.sqrt(num);
-    if(Number.isInteger(sqrt) == true)
-    {
-      bool = false;
-      return bool;
-    }
-    else if(sqrt % 2 == 0)
-    {
-      bool = false;
-      return bool;
-    }
-  }
-  return bool;
-}
+isPrime(49);
 
-isPrime(25)
+/* 
+  Other Solution:
+  function isPrime(num) {
+    for (var i = 2; i < num; i++) if (num % i == 0) return false;
+    return num >= 2; 
+  }
+*/
