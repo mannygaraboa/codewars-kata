39752795e3720ae4348a7660e415a45ec190a5e7
Manuel Garaboa <mannygaraboa@gmail.com>
2019-11-05 18:55:13 -0500

    Rounding kilometers, adding to coordinates, & taking turns function

--- 5kyu/October-2019/SAT-NAV-Directions/index.js
+++ 5kyu/October-2019/SAT-NAV-Directions/index.js
@@ -2,7 +2,6 @@ function satNav(directions) {
   let x = 0;
   let y = 0;
   let nsew = "";
-  let coordinates = [x, y];
 
   for(let i = 0; i < directions.length; i++)
   {
@@ -35,69 +34,320 @@ function satNav(directions) {
     // Going Straight
     else if(directions[i][0] == "G")
     {
+      console.log(directions[i]);
       let direcLength = directions[i].length;
 
       // Moving in kilometers
       if(directions[i][direcLength - 2] == "k")
       {
         let kmString = "";
-        kmString = kmString.concat(directions[i][direcLength - 5], directions[i][direcLength - 4], directions[i][direcLength - 3]);
-        let km = parseFloat(kmString);
-        console.log(km);
+        kmString = kmString.concat(directions[i][direcLength - 5], ".", directions[i][direcLength - 3]);
+        let distance = Math.round(parseFloat(kmString));
+        let distanceString = distance.toString();
+        distanceString = distanceString.concat(0);
+        let finalDistance = parseInt(distanceString);
 
         if(nsew == "N")
         {
-
+          y += finalDistance;
         }
         else if(nsew == "S")
         {
-
+          y -= finalDistance;
         }
         else if(nsew == "E")
         {
-
+          x += finalDistance;
         }
         else if(nsew == "W")
         {
-
+          x -= finalDistance;
         }
       }
       // Moving in meters
       else
       {
         let mString = "";
-        mString = mString.concat(directions[i][direcLength - 4], directions[i][direcLength - 3], directions[i][direcLength - 2]);
-        let m = parseInt(mString, 10);
-        console.log(m);
+        mString = mString.concat(directions[i][direcLength - 4]);
+        let distance = parseInt(mString, 10);
+        console.log(distance);
 
         if(nsew == "N")
         {
-
+          y += distance;
         }
         else if(nsew == "S")
         {
-
+          y -= distance;
         }
         else if(nsew == "E")
         {
-
+          x += distance;
         }
         else if(nsew == "W")
         {
-
+          x -= distance;
         }
       }
+      console.log("Current Coordinates: " + [x,y]);
     }
 
     // Making a Turn
     else if(directions[i][1] == "a")
     {
-
+      console.log(directions[i]);
+      if(directions[i][9] == "N")
+      {
+        if(directions[i][14] == "L")
+        {
+          if(nsew == "N")
+          {
+            y += 10;
+            nsew = "W";
+          }
+          else if(nsew == "S")
+          {
+            y -= 10;
+            nsew = "E";
+          }
+          else if(nsew == "E")
+          {
+            x += 10;
+            nsew = "N";
+          }
+          else if(nsew == "W")
+          {
+            x -= 10;
+            nsew = "S";
+          }
+        }
+        else if(directions[i][14] == "R")
+        {
+          if(nsew == "N")
+          {
+            y += 10;
+            nsew = "E";
+          }
+          else if(nsew == "S")
+          {
+            y -= 10;
+            nsew = "W";
+          }
+          else if(nsew == "E")
+          {
+            x += 10;
+            nsew = "S";
+          }
+          else if(nsew == "W")
+          {
+            x -= 10;
+            nsew = "N";
+          }
+        }
+      }
+      else if(directions[i][9] == "2")
+      {
+        if(directions[i][13] == "L")
+        {
+          if(nsew == "N")
+          {
+            y += 20;
+            nsew = "W";
+          }
+          else if(nsew == "S")
+          {
+            y -= 20;
+            nsew = "E";
+          }
+          else if(nsew == "E")
+          {
+            x += 20;
+            nsew = "N";
+          }
+          else if(nsew == "W")
+          {
+            x -= 20;
+            nsew = "S";
+          }
+        }
+        else if(directions[i][13] == "R")
+        {
+          if(nsew == "N")
+          {
+            y += 20;
+            nsew = "E";
+          }
+          else if(nsew == "S")
+          {
+            y -= 20;
+            nsew = "W";
+          }
+          else if(nsew == "E")
+          {
+            x += 20;
+            nsew = "S";
+          }
+          else if(nsew == "W")
+          {
+            x -= 20;
+            nsew = "N";
+          }
+        }
+      }
+      else if(directions[i][9] == "3")
+      {
+        if(directions[i][13] == "L")
+        {
+          if(nsew == "N")
+          {
+            y += 30;
+            nsew = "W";
+          }
+          else if(nsew == "S")
+          {
+            y -= 30;
+            nsew = "E";
+          }
+          else if(nsew == "E")
+          {
+            x += 30;
+            nsew = "N";
+          }
+          else if(nsew == "W")
+          {
+            x -= 30;
+            nsew = "S";
+          }
+        }
+        else if(directions[i][13] == "R")
+        {
+          if(nsew == "N")
+          {
+            y += 30;
+            nsew = "E";
+          }
+          else if(nsew == "S")
+          {
+            y -= 30;
+            nsew = "W";
+          }
+          else if(nsew == "E")
+          {
+            x += 30;
+            nsew = "S";
+          }
+          else if(nsew == "W")
+          {
+            x -= 30;
+            nsew = "N";
+          }
+        }
+      }
+      else if(directions[i][9] == "4")
+      {
+        if(directions[i][13] == "L")
+        {
+          if(nsew == "N")
+          {
+            y += 40;
+            nsew = "W";
+          }
+          else if(nsew == "S")
+          {
+            y -= 40;
+            nsew = "E";
+          }
+          else if(nsew == "E")
+          {
+            x += 40;
+            nsew = "N";
+          }
+          else if(nsew == "W")
+          {
+            x -= 40;
+            nsew = "S";
+          }
+        }
+        else if(directions[i][13] == "R")
+        {
+          if(nsew == "N")
+          {
+            y += 40;
+            nsew = "E";
+          }
+          else if(nsew == "S")
+          {
+            y -= 40;
+            nsew = "W";
+          }
+          else if(nsew == "E")
+          {
+            x += 40;
+            nsew = "S";
+          }
+          else if(nsew == "W")
+          {
+            x -= 40;
+            nsew = "N";
+          }
+        }
+      }
+      else if(directions[i][9] == "5")
+      {
+        if(directions[i][13] == "L")
+        {
+          if(nsew == "N")
+          {
+            y += 50;
+            nsew = "W";
+          }
+          else if(nsew == "S")
+          {
+            y -= 50;
+            nsew = "E";
+          }
+          else if(nsew == "E")
+          {
+            x += 50;
+            nsew = "N";
+          }
+          else if(nsew == "W")
+          {
+            x -= 50;
+            nsew = "S";
+          }
+        }
+        else if(directions[i][13] == "R")
+        {
+          if(nsew == "N")
+          {
+            y += 50;
+            nsew = "E";
+          }
+          else if(nsew == "S")
+          {
+            y -= 50;
+            nsew = "W";
+          }
+          else if(nsew == "E")
+          {
+            x += 50;
+            nsew = "S";
+          }
+          else if(nsew == "W")
+          {
+            x -= 50;
+            nsew = "N";
+          }
+        }
+      }
+      console.log("Current Coordinates: " + [x,y]);
     }
 
     // Making a U-Turn
     else if(directions[i][1] == "u")
     {
+      console.log(directions[i]);
       if(nsew == "N")
       {
         nsew = "S";
@@ -114,6 +364,7 @@ function satNav(directions) {
       {
         nsew = "E";
       }
+      console.log("Direction: " + nsew);
     }
 
     // Arrived at Destination
@@ -121,13 +372,14 @@ function satNav(directions) {
     {
       console.log("Final Facing Direction: " + nsew);
       console.log("Reached Destination!");
-      return coordinates;
+      return [x,y];
     }
   }
 }
 satNav(["Head WEST", 
         "Take the 2nd Left", 
-        "Go straight on for 200m", 
+        "Go straight on for 200m",
+        "Take the NEXT Right", 
         "Go straight on for 1.5km", 
         "Turn around!",
         "You have reached your destination!"]);
