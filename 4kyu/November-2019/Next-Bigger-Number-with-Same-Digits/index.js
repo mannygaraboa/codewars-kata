e15314d1bddf6b3ea2d71228cde19c5f80b0e955
Manuel Garaboa <mannygaraboa@gmail.com>
2019-11-17 23:15:14 -0500

    Next Bigger Number with Same Digits kata solution

--- 4kyu/November-2019/Next-Bigger-Number-with-Same-Digits/index.js
+++ 4kyu/November-2019/Next-Bigger-Number-with-Same-Digits/index.js
@@ -33,6 +33,7 @@ function nextBigger(n){
               if(nArray[i] < nArray[i - 1])
               {
                 let spliced = nArray.splice(i - 1);
+                nArray.sort();
                 let finalArray = spliced.concat(nArray);
                 let finalNum = parseInt(finalArray.join(""));
                 return finalNum;
@@ -41,11 +42,17 @@ function nextBigger(n){
           }
           else if(nArray[0] > nArray[nArray.length - 1])
           {
-            let spliced = nArray.splice(nArray.length - 2);
-            nArray.sort();
-            let finalArray = spliced.concat(nArray);
-            let finalNum = parseInt(finalArray.join(""));
-            return finalNum;
+            for(let i = 0; i < nArray.length; i++)
+            {
+              if(nArray[0] < nArray[(nArray.length - 1) - i])
+              {
+                let spliced = nArray.splice(((nArray.length - 1) - i), 1);
+                nArray.sort();
+                let finalArray = spliced.concat(nArray);
+                let finalNum = parseInt(finalArray.join(""));
+                return finalNum;
+              }
+            }
           }
           else if(nArray[0] < nArray[nArray.length - 1])
           {
@@ -59,24 +66,31 @@ function nextBigger(n){
         else
         {
           let spliced = nArray.splice(i);
-          let removedNum = parseInt(spliced.splice(1, 1));
-          spliced.sort();
-          spliced.unshift(removedNum);
-          let finalArray = nArray.concat(spliced);
-          let finalNum = parseInt(finalArray.join(""));
-          return finalNum;
+          for(let i = 0; i < spliced.length; i++)
+          {
+            if(spliced[0] < spliced[(spliced.length - 1) - i])
+            {
+              let splicedNum = spliced.splice(((spliced.length - 1) - i), 1);
+              spliced.sort();
+              spliced.unshift(splicedNum);
+              let finalArray = nArray.concat(spliced);
+              let finalNum = parseInt(finalArray.join(""));
+              return finalNum;
+            }
+          }
         }
       }
     }
   }
 }
-nextBigger(153931);
+nextBigger(59884848459853);
 
 // 2710 ==> 7012
 // 153 ==> 315
 // 1541 ==> 4115
 // 1542 ==> 2145
 // 153931 ==> 159133
+// 192111 ==> 211119
 // 198761 ==> 611789
 // 298761 ==> 612789
 // 298763 ==> 326789
@@ -85,3 +99,29 @@ nextBigger(153931);
 // 191112 ==> 191121
 // 59876 ==> 65789
 // 59786 ==> 59867
+// 59884848459853 ==> 59884848483559
+
+// Other Solution:
+/*
+  function nextBigger(n){
+    console.log(n);
+    var chars = n.toString().split('');
+    var i = chars.length-1;
+    while(i > 0) {
+      if (chars[i]>chars[i-1]) break;
+      i--;
+    }
+    if (i == 0) return -1;
+    var suf = chars.splice(i).sort();
+    var t = chars[chars.length-1];
+    for (i = 0; i < suf.length; ++i) {
+      if (suf[i] > t) break;
+    }
+    chars[chars.length-1] = suf[i]
+    suf[i] = t;
+    var res = chars.concat(suf);
+    var num = parseInt(res.join(''));
+    console.log("->" +num);
+    return num;
+  }
+*/
