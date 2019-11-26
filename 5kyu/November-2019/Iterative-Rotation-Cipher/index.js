4d14408ce14cc73702e86116bb52c63bc93d2d2d
Manuel Garaboa <mannygaraboa@gmail.com>
2019-11-25 23:55:44 -0500

    Step 1 of Encode function

--- 5kyu/November-2019/Iterative-Rotation-Cipher/index.js
+++ 5kyu/November-2019/Iterative-Rotation-Cipher/index.js
@@ -5,7 +5,26 @@ const IterativeRotationCipher = {};
 /*--------------------------------------------------------------------------------------*/
 
 IterativeRotationCipher.encode = function(n,str){
-	
+	// (1.) Remove spaces in string (Note positions of spaces)
+  let spaceArray = [];
+  for(let i = 0; i < str.length; i++)
+  {
+    if(str.charAt(i) == " ")
+    {
+      spaceArray.push(i);
+      str = str.replace(" ", "");
+    }
+  }
+
+  // (2.) Shift order of characters of new string to the right by "n" characters
+
+
+  // (3.) Put spaces back in original positions
+
+
+  // (4.) Shift characters of each substring (separated by 1 or more consecutive spaces) to the right
+
+
 };
 
 /*--------------------------------------------------------------------------------------*/
