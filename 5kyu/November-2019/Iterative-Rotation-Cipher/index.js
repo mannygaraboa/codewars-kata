c91cd99cff9f1ed2f8f5ac7e67d8d33e5d63956b
Manuel Garaboa <mannygaraboa@gmail.com>
2019-12-08 23:59:44 -0500

    Progress

--- 5kyu/November-2019/Iterative-Rotation-Cipher/index.js
+++ 5kyu/November-2019/Iterative-Rotation-Cipher/index.js
@@ -1,50 +1,64 @@
 const IterativeRotationCipher = {};
 let quote = `If you wish to make an apple pie from scratch, you must first invent the universe.`;
+let solution = `10 hu fmo a,ys vi utie mr snehn rni tvte .ysushou teI fwea pmapi apfrok rei tnocsclet`;
+// IterativeRotationCipher.encode(10,quote) === solution; //true
 
 /*--------------------------------------------------------------------------------------*/
 /*----------------------------------------ENCODE----------------------------------------*/
 /*--------------------------------------------------------------------------------------*/
 
 IterativeRotationCipher.encode = function(n,str){
-	// (1.) Remove spaces in string (Note positions of spaces)
-  let spaceArray = [];
-  for(let i = 0; i < str.length; i++)
+  // Must repeat cycle of steps the same number of times as n
+  for(let i = 0; i < n; i++)
   {
-    if(str.charAt(i) == " ")
+    // (1.) Remove spaces in string (Note positions of spaces)
+    let spaceArray = [];
+    for(let i = 0; i < str.length; i++)
     {
-      spaceArray.push(i);
-      str = str.replace(" ", "");
+      if(str.charAt(i) == " ")
+      {
+        console.log(i);
+        spaceArray.push(i);
+      }
+    }
+    for(let i = 0; i < str.length; i++)
+    {
+      if(str.charAt(i) == " ")
+      {
+        str = str.replace(" ", "");
+      }
+    }
+    console.log(str);
+    console.log("Spaces Positions: " + spaceArray);
+
+    // (2.) Shift order of characters of new string to the right by "n" characters
+    let shiftString = "";
+    for(let i = str.length - n; i < str.length; i++)
+    {
+      shiftString = shiftString.concat(str.charAt(i));
+    }
+    str = str.replace(shiftString, "");
+    let newShiftString = shiftString.concat(str);
+    console.log(newShiftString);
+    
+    // (3.) Put spaces back in original positions
+    let charArray = [];
+    for(let i = 0; i < newShiftString.length; i++)
+    {
+      charArray.push(newShiftString.charAt(i));
+    }
+    for(let i = 0; i < spaceArray.length; i++)
+    {
+      charArray.splice(spaceArray[i], 0, " ");
+    }
+    console.log(charArray);
+
+    // (4.) Shift characters of each substring (separated by 1 or more consecutive spaces) to the right
+    for(let i = 0; i < charArray.length; i++)
+    {
+      
     }
   }
-
-  // (2.) Shift order of characters of new string to the right by "n" characters
-  let shiftString = "";
-  for(let i = str.length - n; i < str.length; i++)
-  {
-    shiftString = shiftString.concat(str.charAt(i));
-  }
-  str = str.replace(shiftString, "");
-  let newShiftString = shiftString.concat(str);
-  // console.log(newShiftString);
-
-  // (3.) Put spaces back in original positions
-  let charArray = [];
-  for(let i = 0; i < newShiftString.length; i++)
-  {
-    charArray.push(newShiftString.charAt(i));
-  }
-  for(let i = 0; i < spaceArray.length; i++)
-  {
-    charArray.splice(spaceArray[i], 0, " ");
-  }
-  // console.log(charArray);
-
-  // (4.) Shift characters of each substring (separated by 1 or more consecutive spaces) to the right
-  for(let i = 0; i < charArray.length; i++)
-  {
-
-  }
-
 };
 
 /*--------------------------------------------------------------------------------------*/
