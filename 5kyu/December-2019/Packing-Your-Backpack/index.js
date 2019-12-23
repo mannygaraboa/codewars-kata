d6cdfab55e7d7192a830a2f2dd29a4a9d71d87f6
Manuel Garaboa <mannygaraboa@gmail.com>
2019-12-22 23:44:28 -0500

    Packing your Backpack kata start

---
+++ 5kyu/December-2019/Packing-Your-Backpack/index.js
@@ -0,0 +1,24 @@
+function packBagpack(scores, weights, capacity) {
+  let scoreArray = [];
+  for(let i = 0; i < weights.length; i++)
+  {
+    if(weights[i] <= capacity)
+    {
+      scoreArray.push(scores[i]);
+    }
+  }
+  return scoreArray;
+}
+packBagpack([15, 10, 9, 5], [1, 5, 3, 4], 3)
+
+// scores = [15, 10, 9, 5]
+// weights = [1, 5, 3, 4]
+// capacity = 8
+
+// Solution is 29 (items 1, 3 & 4)
+
+// scores = [4, 12, 27, 31, 5]
+// weights = [1, 5, 3, 4, 6]
+// capacity = 15
+
+// Solution is 
